458ac20dbf8500969ead67be34ba9ab44e614f59
ACM-Network <rayuduv721@gmail.com>
2026-05-03 12:01:58 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,7 +3,7 @@ from http.server import BaseHTTPRequestHandler
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
+        self.send_header("Content-type", "text/plain")
         self.end_headers()
 
-        self.wfile.write(b"Jarvis backend is running 🚀")
+        self.wfile.write(b"Jarvis backend running successfully 🚀")

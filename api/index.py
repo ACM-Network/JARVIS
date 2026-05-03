411cdf4cb49e529eb5ed538f78be4d544ea082d2
ACM-Network <rayuduv721@gmail.com>
2026-05-03 12:13:08 +0530

    Refactor app function response message

--- api/index.py
+++ api/index.py
@@ -1,14 +1,8 @@
+# DO NOT IMPORT ANYTHING
+
 def app(request):
-    try:
-        return {
-            "statusCode": 200,
-            "headers": {
-                "Content-Type": "text/plain"
-            },
-            "body": "Jarvis backend is alive 🚀"
-        }
-    except Exception as e:
-        return {
-            "statusCode": 500,
-            "body": str(e)
-        }
+    return {
+        "statusCode": 200,
+        "headers": {"Content-Type": "text/plain"},
+        "body": "Jarvis API working perfectly 🚀"
+    }

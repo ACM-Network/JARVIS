23982dcff9726d6a0bcd9e636b779918c3638d5e
ACM-Network <rayuduv721@gmail.com>
2026-05-03 12:16:49 +0530

    Update API response body message

--- api/index.py
+++ api/index.py
@@ -1,8 +1,6 @@
-# DO NOT IMPORT ANYTHING
-
 def app(request):
     return {
         "statusCode": 200,
         "headers": {"Content-Type": "text/plain"},
-        "body": "Jarvis API working perfectly 🚀"
+        "body": "Jarvis API working 🚀"
     }

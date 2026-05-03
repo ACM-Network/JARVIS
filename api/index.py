7f0a13bca0fb05fae4c1b52f0c620366d230170a
ACM-Network <rayuduv721@gmail.com>
2026-05-03 12:09:28 +0530

    Rename handler function to app

--- api/index.py
+++ api/index.py
@@ -1,5 +1,8 @@
-def handler(request):
+def app(request):
     return {
         "statusCode": 200,
+        "headers": {
+            "Content-Type": "text/plain"
+        },
         "body": "Jarvis backend running successfully 🚀"
     }
